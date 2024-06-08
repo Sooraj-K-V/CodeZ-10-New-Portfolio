e0c3a2e9573c2540c59dd0a33b12c5860c07ab71
Sooraj K V <144308480+Sooraj-K-V@users.noreply.github.com>
2024-06-09 01:17:19 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+    $(window).on('scroll', function () {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 50) {
+            $(".sticky").addClass("stickyadd");
+        } else {
+            $(".sticky").removeClass("stickyadd");
+        }
+    })
+
+    var typed = new Typed('.element', {
+        strings: ['Sooraj K V', 'A Developer'],
+        smarBackspace: true,
+        typeSpeed: 100,
+        backspeed: 100,
+        loop: true,
+        loopCount: Infinity,
+        startDelay: 1000
+    });
+
+    $(".btn").click(function () {
+        notify("CV Not Uploaded :)");
+    });
+
+    // progress-bar
+ 
+    var waypoint = new Waypoint({
+        element: document.getElementById('exp-id'),
+        handler: function () {
+            var p = document.querySelectorAll(".progress-bar");
+            p[0].setAttribute("style", "width:95%; transition:1s all;");
+            p[1].setAttribute("style", "width:60%; transition:1.5s all;");
+            p[2].setAttribute("style", "width:60%; transition:2s all;");
+            p[3].setAttribute("style", "width:50%; transition:2.5s all;");
+            p[4].setAttribute("style", "width:65%; transition:3s all;");
+            p[5].setAttribute("style", "width:20%; transition:3.5s all;");
+            p[6].setAttribute("style", "width:5%; transition:4s all;");
+
+        },
+        offset: '90%'
+    });
+
+    // owl carousel
+
+    $(document).ready(function () {
+        $(".owl-carousel").owlCarousel({
+            loop:true,
+            autoplay:true,
+            autoplayTimeout:4000,
+            items:1
+        });
+    });
+
+});
